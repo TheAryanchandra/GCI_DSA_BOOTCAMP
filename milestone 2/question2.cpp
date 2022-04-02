2224ad59ee673aeb769f024eb11c856c106e1348
Aryan chandra <90573146+TheAryanchandra@users.noreply.github.com>
2022-04-02 19:51:37 +0530

    milestone 2 completed

---
+++ milestone 2/question2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+   
+    cin>>n;
+    int i=n;
+ while(i>=0){
+     cout<<i<<endl;
+     i--;
+ }
+
+}
---
+++ milestone 2/question3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+        
+    char i;
+    cout << "The Alphabets from A to Z are:"<<endl;
+    for (i = 'A'; i <= 'Z'; i++)
+    {    cout <<i<<endl;
+    }
+    return 0;
+}
